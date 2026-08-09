b097a7a884e0563b07fe0e022e5e23057a5fd412
khaledtahapro-prog <khaled.taha.pro@gmail.com>
2026-08-09 20:17:34 +0300

    Update header.php

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -9,6 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- FontAwesome Icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <!-- Custom CSS -->
     <link rel="stylesheet" href="<?php echo base_url('assets/css/style.css'); ?>">
 </head>
